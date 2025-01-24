609fdd31d11ba733a24e494e13839c6eba2d3b74
Chris <59448319+rorocc@users.noreply.github.com>
2025-01-24 20:39:21 +0100

    its elgigandet time

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,21 @@
 <script setup>
-
+  import Clock from '@/components/Clock.vue';
 </script>
 
 <template>
   <main>
     <img alt="Lootdoolfs logo" class="logo" src="@/assets/lootis.png"/>
+    <Clock class="clock" />
   </main>
 </template>
 
 <style scoped>
+.clock{
+  width: 95%;
+}
+
 .logo {
-  display: block;
+  display: none;
   width: 50%;
   user-select: none;
   -webkit-user-drag: none;
@@ -26,6 +31,9 @@ main{
 @media (min-width: 1024px) {
   .logo{
     width: 35%;
+  }
+  .clock{
+    width: 50%;
   }
 }
 
